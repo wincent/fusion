b52563553e8b38b88c84702ddb546ea810d63121
Wincent Colaiuta <win@wincent.com>
2010-11-07 13:53:00 +0100

    First cut at basic dependency resolution
    Based on an algorithm described in this blog post:
    
      http://www.electricmonk.nl/log/2008/08/07/dependency-resolving-algorithm/
    
    Chosen for its simplicity, given that extreme efficiency is unlikely to
    be needed with the anticipated working set sizes (and if it ever does
    become an issue, a more efficient algorithm can be investigated).
    
    Note: the blog post in question makes the issue seem more simple than it
    really is as it says, "we'll start with node 'A'", which just happens to
    be the only node in the graph which will result in the dependencies
    being fully resolved. Starting from any other node will result in an
    incomplete dependency graph. So, we need to iterate over all nodes,
    applying the algorithm to each in turn, to ensure that we get a complete
    graph.
    
    Other algorithms are mentioned here:
    
      http://en.wikipedia.org/wiki/Topological_sorting
    
    Signed-off-by: Wincent Colaiuta <win@wincent.com>

--- WOFPlugInManager.m
+++ WOFPlugInManager.m
@@ -52,7 +52,61 @@
 //! @see    #searchPaths
 - (void)findAllPlugIns;
 
+//! Removes duplicate plug-ins (plug-ins in different locations but with
+//! identical identifiers), prioritizing plug-ins found earlier on in the search
+//! path.
+//!
+//! @see #findAllPlugIns
+- (void)determineUniquePlugIns;
+
+//! Examines the declared dependencies of the set of unique plug-ins, and
+//! establishes an order in which they should be loaded that will guarantee that
+//! each plug-in's dependencies are already available by the time it is loaded.
+//!
+//! @exception  WOFCircularDependencyException thrown if a circular dependency
+//!             is detected.
+//! @note       There may be multiple valid load orders that would satisfy all
+//!             dependencies; the load order that is determined by this method
+//!             arises from the order in which plug-ins are listed on the disk.
+- (void)resolveDependencies;
+
+//! Recusive helper method that is used during the dependency resolution
+//! process.
+//!
+//! @exception  WOFCircularDependencyException thrown if a circular dependency
+//!             is detected.
+- (void)resolveDependenciesForPlugIn:(WOFPlugIn *)aPlugIn
+                            resolved:(NSMutableArray *)resolved
+                          unresolved:(NSMutableSet *)unresolved;
+
+//! An ordered list of plug-ins as found on disk.
+//!
+//! Locations are searched in the order returned by
+//! NSSearchPathForDirectoriesInDomains(), which means that plug-ins which are
+//! "closer" (for example, embedded inside the host application itself, or in
+//! the user's home directory) will appear before plug-ins found elsewhere (in
+//! /Library, for instance).
+//!
+//! This allows us to establish a precendence mechanism so that, given two
+//! plug-ins in different locations but with the same bundle identifier, we
+//! prefer the most proximal.
 @property(readwrite, copy) NSArray *plugIns;
+
+//! A dictionary of unique plug-ins, using bundle identifiers as keys and
+//! plug-in instances as values, produced by removing duplicate plug-ins from
+//! the plugIns list.
+//!
+//! When multiple plug-ins share the same identifier, the most proximal plug-in
+//! is selected for membership in the uniquePlugIns set.
+@property(readwrite, copy) NSDictionary *uniquePlugIns;
+
+//! An ordered list of plug-ins with dependencies resolved.
+//!
+//! Plug-ins are ordered such that those which are dependencies of other
+//! plug-ins appear earlier in the list. As such, the resolvedPlugIns list
+//! effectively indicates the order in which plug-ins should be loaded so as to
+//! guarantee that all dependencies are met.
+@property(readwrite, copy) NSArray *resolvedPlugIns;
 
 @end
 
@@ -72,7 +126,10 @@
 - (id)init
 {
     if ((self = [super init]))
+    {
         [self findAllPlugIns];
+        [self determineUniquePlugIns];
+    }
     return self;
 }
 
@@ -118,9 +175,60 @@
     self.plugIns = plugIns;
 }
 
+- (void)determineUniquePlugIns
+{
+    NSMutableDictionary *entries = [NSMutableDictionary dictionaryWithCapacity:[self.plugIns count]];
+    for (WOFPlugIn *plugIn in self.plugIns)
+    {
+        NSString *identifier = [plugIn bundleIdentifier];
+        if (![entries objectForKey:identifier])
+            [entries setObject:plugIn forKey:identifier];
+    }
+    self.uniquePlugIns = entries;
+}
+
+- (void)resolveDependencies
+{
+    NSUInteger count = [self.uniquePlugIns count];
+    NSMutableArray *resolved = [NSMutableArray arrayWithCapacity:count];
+    for (NSString *identifier in self.uniquePlugIns)
+    {
+        [self resolveDependenciesForPlugIn:[self plugInForIdentifier:identifier]
+                                  resolved:resolved
+                                unresolved:[NSMutableSet setWithCapacity:count]];
+    }
+    self.resolvedPlugIns = resolved;
+}
+
+- (void)resolveDependenciesForPlugIn:(WOFPlugIn *)aPlugIn
+                            resolved:(NSMutableArray *)resolved
+                          unresolved:(NSMutableSet *)unresolved
+{
+    [unresolved addObject:aPlugIn];
+    for (NSString *identifier in aPlugIn.dependencies)
+    {
+        WOFPlugIn *dependency = [self plugInForIdentifier:identifier];
+        if (![resolved containsObject:dependency])
+        {
+            if ([unresolved containsObject:dependency])
+                [NSException raise:@"WOFCircularDependencyException"
+                            format:@"plug-ins %@ and %@ form part of a circular dependency",
+                 aPlugIn, dependency];
+            [self resolveDependenciesForPlugIn:dependency
+                                      resolved:resolved
+                                    unresolved:unresolved];
+        }
+    }
+    [resolved addObject:aPlugIn];
+    [unresolved removeObject:aPlugIn];
+}
+
 - (NSError *)loadAllPlugIns
 {
-    for (WOFPlugIn *plugIn in self.plugIns)
+    if (!self.resolvedPlugIns)
+        [self resolveDependencies];
+
+    for (WOFPlugIn *plugIn in self.resolvedPlugIns)
     {
         NSError *error;
         if (![plugIn loadAndReturnError:&error])
@@ -132,8 +240,7 @@
 
 - (WOFPlugIn *)plugInForIdentifier:(NSString *)anIdentifier
 {
-    // TODO: implementation
-    return nil;
+    return [self.uniquePlugIns objectForKey:anIdentifier];
 }
 
 - (WOFPlugIn *)plugInForClass:(Class)aClass
@@ -143,5 +250,7 @@
 }
 
 @synthesize plugIns;
+@synthesize uniquePlugIns;
+@synthesize resolvedPlugIns;
 
 @end
